1a57994e7010691a16ccb31ccb0cd46d0b748d3b
JCollinJones25 <jamescollin.jones.jcj@gmail.com>
2022-07-19 22:48:00 -0400

    db changed back to localhost

--- db.js
+++ db.js
@@ -1,17 +1,17 @@
 const Pool = require('pg').Pool;
 
-// const pool = new Pool({
-//     user: "CollinJones",
-//     password: "collinjones",
-//     host: "localhost",
-//     port: 5432,
-//     database: "trips"
-// })
-
-const connectStr = process.env.DATABASE_URL;
 const pool = new Pool({
-    connectionString: connectStr,
-    ssl: true
-});
+    user: "CollinJones",
+    password: "collinjones",
+    host: "localhost",
+    port: 5432,
+    database: "trips"
+})
+
+// const connectStr = process.env.DATABASE_URL;
+// const pool = new Pool({
+//     connectionString: connectStr,
+//     ssl: true
+// });
 
 module.exports = pool;
