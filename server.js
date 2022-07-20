7909bf5c5106e6c3c1a5c78f812bd677f1a1e980
JCollinJones25 <jamescollin.jones.jcj@gmail.com>
2022-07-20 14:42:09 -0400

    changes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
+const app = express()
 const cors = require('cors')
 require('dotenv').config()
-const app = express()
 const PORT = process.env.PORT
 const pool = require('./db')
 
