ca759f86f16133b764bbb56abeaf7469050548b4
JCollinJones25 <jamescollin.jones.jcj@gmail.com>
2022-08-02 14:22:14 -0400

    trying to solve cors error

--- server.js
+++ server.js
@@ -9,12 +9,15 @@ const pool = require('./db')
 //MIDDLEWARE
 app.use(cors())
 app.use(express.json())
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("public"))
 } 
-
-
 
 //ROUTES
 
