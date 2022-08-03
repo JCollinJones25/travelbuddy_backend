3968567e369712289e3eb170ac25cf0e51af0eed
JCollinJones25 <jamescollin.jones.jcj@gmail.com>
2022-08-03 16:30:01 -0400

    db.js altered

--- db.js
+++ db.js
@@ -3,20 +3,10 @@ require("dotenv").config();
 
 const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PH_PORT}/${process.env.PG_DATABASE}`
 
-// {
-//     user: process.env.PG_USER,
-//     password: process.env.PG_PASSWORD,
-//     host: process.env.PG_HOST,
-//     port: process.env.PG_PORT,
-//     database: process.env.PG_DATABASE,
-//     ssl: true
-// }
-
 const proConfig = process.env.DATABASE_URL
 
 const pool = new Pool({
-    connectionString: process.env.NODE_ENV === "production" ? proConfig : devConfig,
-    ssl: true
+    connectionString: process.env.NODE_ENV === "production" ? proConfig : devConfig
 })
 
 
