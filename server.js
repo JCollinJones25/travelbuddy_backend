8a8732c9dfe31a9e2c7d8dd0c26922bd7348fd24
JCollinJones25 <jamescollin.jones.jcj@gmail.com>
2022-08-02 20:43:26 -0400

    urlencoded changed to false

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const pool = require('./db')
 //MIDDLEWARE
 app.use(cors())
 app.use(express.json())
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
